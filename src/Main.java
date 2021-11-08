2f37e9bb6fa86dd96b9b8d617e88422f60ae3db3
xRuiAlves <ruialves.esrt.98@gmail.com>
2021-11-08 20:39:04 +0000

    Add main function

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+import days.Day1;
+import days.MultipartDaySolution;
+
+import java.util.List;
+
+public class Main {
+    private static final List<MultipartDaySolution> solutions = List.of();
+
+    public static void main(String[] args) {
+        solutions.forEach(System.out::println);
+    }
+}

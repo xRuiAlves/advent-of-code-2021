86b269ccdd9ad272d184a3447b56ae073a671cdf
xRuiAlves <ruialves.esrt.98@gmail.com>
2021-12-02 08:13:40 +0000

    Add day 2 solution

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,14 @@
 import days.MultipartDaySolution;
 import days.day1.Day1;
+import days.day1.Day2;
 
 import java.util.List;
 
 public class Main {
-    private static final List<MultipartDaySolution> solutions = List.of(new Day1("input/day1.txt"));
+    private static final List<MultipartDaySolution> solutions = List.of(
+            new Day1("input/day1.txt"),
+            new Day2("input/day2.txt")
+    );
 
     public static void main(String[] args) {
         solutions.forEach(System.out::println);

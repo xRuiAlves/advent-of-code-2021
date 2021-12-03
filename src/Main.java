c54464c4c7caf24e7833b0778548821a0e706352
Rui Alves <ralves@palantir.com>
2021-12-03 09:12:19 +0000

    Add day 3 solution

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,15 @@
 import days.MultipartDaySolution;
 import days.day1.Day1;
 import days.day1.Day2;
+import days.day1.Day3;
 
 import java.util.List;
 
 public class Main {
     private static final List<MultipartDaySolution> solutions = List.of(
             new Day1("input/day1.txt"),
-            new Day2("input/day2.txt")
+            new Day2("input/day2.txt"),
+            new Day3("input/day3.txt")
     );
 
     public static void main(String[] args) {

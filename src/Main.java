3119ad9b5a9e740eea6194f72e64f02e0591afb0
Rui Alves <ralves@palantir.com>
2021-12-07 18:13:35 +0000

    Add day 7 solution

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,8 @@ import days.day2.Day2;
 import days.day3.Day3;
 import days.day4.Day4;
 import days.day5.Day5;
+import days.day6.Day6;
+import days.day7.Day7;
 
 import java.util.List;
 
@@ -13,7 +15,9 @@ public class Main {
             new Day2("input/day2.txt"),
             new Day3("input/day3.txt"),
             new Day4("input/day4.txt"),
-            new Day5("input/day5.txt")
+            new Day5("input/day5.txt"),
+            new Day6("input/day6.txt"),
+            new Day7("input/day7.txt")
     );
 
     public static void main(String[] args) {

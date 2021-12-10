637b5006b0e8797fa3ac39e7b62cb58969733308
Rui Alves <ralves@palantir.com>
2021-12-10 13:56:51 +0000

    Add day 10 solution

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 import days.MultipartDaySolution;
 import days.day1.Day1;
+import days.day10.Day10;
 import days.day2.Day2;
 import days.day3.Day3;
 import days.day4.Day4;
@@ -21,7 +22,8 @@ public class Main {
             new Day6("input/day6.txt"),
             new Day7("input/day7.txt"),
             new Day8("input/day8.txt"),
-            new Day9("input/day9.txt")
+            new Day9("input/day9.txt"),
+            new Day10("input/day10.txt")
     );
 
     public static void main(String[] args) {

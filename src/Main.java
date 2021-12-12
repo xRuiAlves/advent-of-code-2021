c387c6d5d12aaa31149f786fb4720279ea5eb336
xRuiAlves <ruialves.esrt.98@gmail.com>
2021-12-12 18:15:12 +0000

    Add day 12 solution

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ import days.MultipartDaySolution;
 import days.day1.Day1;
 import days.day10.Day10;
 import days.day11.Day11;
+import days.day12.Day12;
 import days.day2.Day2;
 import days.day3.Day3;
 import days.day4.Day4;
@@ -25,7 +26,8 @@ public class Main {
             new Day8("input/day8.txt"),
             new Day9("input/day9.txt"),
             new Day10("input/day10.txt"),
-            new Day11("input/day11.txt")
+            new Day11("input/day11.txt"),
+            new Day12("input/day12.txt")
     );
 
     public static void main(String[] args) {

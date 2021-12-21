d0c28a015f6710e8540c34c2677ae6f626479b17
xRuiAlves <ruialves.esrt.98@gmail.com>
2021-12-21 17:36:58 +0000

    Add day 21 solution

--- src/Main.java
+++ src/Main.java
@@ -12,6 +12,7 @@ import days.day18.Day18;
 import days.day19.Day19;
 import days.day2.Day2;
 import days.day20.Day20;
+import days.day21.Day21;
 import days.day3.Day3;
 import days.day4.Day4;
 import days.day5.Day5;
@@ -42,7 +43,9 @@ public class Main {
             new Day16("input/day16.txt"),
             new Day17("input/day17.txt"),
             new Day18("input/day18.txt"),
-            new Day19("input/day19.txt")
+            new Day19("input/day19.txt"),
+            new Day20("input/day20.txt"),
+            new Day21("input/day21.txt")
     );
 
     public static void main(String[] args) {

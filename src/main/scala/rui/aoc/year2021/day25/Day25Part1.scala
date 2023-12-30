791c6010e41609724a828fec53c677fcdbc42825
xRuiAlves <ruialves.esrt.98@gmail.com>
2023-12-30 17:22:28 +0000

    Improve day 25

--- src/main/scala/rui/aoc/year2021/day25/Day25Part1.scala
+++ src/main/scala/rui/aoc/year2021/day25/Day25Part1.scala
@@ -10,31 +10,27 @@ class Day25Part1 extends ProblemSolution {
   private[this] final val RightFacingCell = '>'
   private[this] final val DownFacingCell = 'v'
 
-  override def solve(): AnyVal = {
-    val map = FileIO.readResourceLines("day25.txt").map(_.toCharArray)
-    step(map)
-  }
+  override def solve(): AnyVal = step(FileIO.readResourceLines("day25.txt").map(_.toCharArray))
 
   @tailrec
   private final def step(map: Array[Array[Char]], stepNumber: Int = 1): Int = {
-    var moved = false
     val cells = (for (i <- map.indices; j <- map(i).indices) yield (i, j)).toSet
 
-    val toMoveHorizontally = cells.filter { case (i, j) => map(i)(j) == RightFacingCell && map(i)((j + 1) % map(i).length) == EmptyCell }
-    toMoveHorizontally.foreach { case (i, j) =>
-      moved = true
-      map(i)(j) = EmptyCell
-      map(i)((j + 1) % map(i).length) = RightFacingCell
+    def move(cellType: Char, deltaI: Int, deltaJ: Int): Boolean = {
+      val toMove = cells.filter { case (i, j) => map(i)(j) == cellType &&
+        map((i + deltaI) % map.length)((j + deltaJ) % map(i).length) == EmptyCell
+      }
+      toMove.foreach { case (i, j) =>
+        map(i)(j) = EmptyCell
+        map((i + deltaI) % map.length)((j + deltaJ) % map(i).length) = cellType
+      }
+      toMove.nonEmpty
     }
 
-    val toMoveVertically = cells.filter { case (i, j) => map(i)(j) == DownFacingCell && map((i + 1) % map.length)(j) == EmptyCell }
-    toMoveVertically.foreach { case (i, j) =>
-      moved = true
-      map(i)(j) = EmptyCell
-      map((i + 1) % map.length)(j) = DownFacingCell
-    }
+    val movedRight = move(RightFacingCell, 0, 1)
+    val movedDown = move(DownFacingCell, 1, 0)
 
-    if (moved) step(map, stepNumber + 1)
+    if (movedRight || movedDown) step(map, stepNumber + 1)
     else stepNumber
   }
 }
